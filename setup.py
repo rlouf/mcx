e81b1dd0284b779c32c188df12fecc3a4e598d3f
Rémi Louf <remilouf@gmail.com>
2020-01-15 19:17:34 +0100

    add requirements

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import setuptools
 
 setuptools.setup(
-    name="rax",
+    name="mcmx",
     version="0.0.1",
     author="Rémi Louf",
     author_email="remilouf@gmail.com",
     description="Yet Another Probabilistic Programming Library",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/rlouf/rax",
+    url="https://github.com/rlouf/mcmx",
     licence="Apache Licence Version 2.0",
-    packages=["rax"],
+    packages=["mcmx"],
     python_requires=">=3.5",
     install_requires=["numpy", "jax", "jaxlib"],
 )
