b8fdeb552e81479e7657d2707fc59b74bd099808
Rémi Louf <remilouf@gmail.com>
2020-01-25 13:11:08 +0100

    add tests for categorical distribution
    The tests currently do not pass; `jax.random.categorical` is implemented
    in the master branch of the repo, but has not been released yet. Given
    that it will probably be released by the time this library is in a
    usable state, we implement tests that will break until the function
    appears in a future release of JAX.

--- mcmx/distributions/categorical.py
+++ mcmx/distributions/categorical.py
@@ -1,25 +1,27 @@
-from jax import nn
 from jax import numpy as np
 from jax import random
+from jax.scipy.special import xlogy
 
 from . import constraints
 from .distribution import Distribution
+from .utils import limit_to_support
 
 
 class Categorical(Distribution):
-    param_constraints = {"logits": constraints.real}
+    param_constraints = {"probs": constraints.simplex}
 
-    def __init__(self, logits):
-        self.support = constraints.integer_interval(0, logits.shape[-1])
+    def __init__(self, probs):
+        self.support = constraints.integer_interval(0, probs.shape[-1] - 1)
 
         self.event_shape = ()
-        self.batch_shape = logits.shape[:-1]
-        self.logits = logits
+        self.batch_shape = probs.shape[:-1]
+        self.probs = probs
 
     def sample(self, rng_key, sample_shape):
         shape = sample_shape + self.batch_shape + self.event_shape
-        return random.categorical(rng_key, self.logits, axis=0, shape=shape)
+        return random.categorical(rng_key, self.probs, axis=0, shape=shape)
 
+    @limit_to_support
     def logpdf(self, x):
-        log_p = nn.log_softmax(self.logits, axis=0)
-        return np.sum(x * log_p)
+        x_array = np.arange(self.probs.shape[-1]) == x
+        return np.sum(xlogy(x_array, self.probs), axis=-1)
---
+++ tests/categorical_test.py
@@ -0,0 +1,121 @@
+import unittest
+
+from jax import numpy as np
+from jax import random
+import scipy
+
+from mcmx.distributions import Categorical
+
+
+class CategoricalTest(unittest.TestCase):
+    def setUp(self):
+        self.rng_key = random.PRNGKey(0)
+
+    #
+    # SAMPLING CORRECTNESS
+    #
+
+    def test_sample_mode(self):
+        test_cases = [
+            {"probs": np.array([0, 1.0]), "mode": 1},
+            {"probs": np.array([0.501, 0.499]), "mode": 0},
+            {"probs": np.array([0.2, 0.7, 0.1]), "mode": 1},
+            {"probs": np.array([1.0]), "mode": 0},
+        ]
+        for case in test_cases:
+            samples = Categorical(case["probs"]).sample(self.rng_key, (100_000,)).__array__()
+            mode = scipy.stats.mode(samples, axis=0)
+            self.assertEqual(mode, case["mode"])
+
+    #
+    # LOGPDF CORRECTNESS
+    #
+
+    def test_logpdf_out_of_support(self):
+        test_cases = [
+            {"probs": np.array([0.1, 0.2, 0.7]), "x": -1, "expected": -np.inf},
+            {"probs": np.array([0.1, 0.2, 0.7]), "x": 3, "expected": -np.inf},
+            {"probs": np.array([0.1, 0.2, 0.7]), "x": 3.5, "expected": -np.inf},
+        ]
+        for case in test_cases:
+            logprob = Categorical(case["probs"]).logpdf(case["x"])
+            self.assertEqual(logprob, case["expected"])
+
+    def test_logpdf_edge_cases(self):
+        test_cases = [
+            {"probs": np.array([1]), "x": 0, "expected": 0.0},
+            {"probs": np.array([0, 1]), "x": 1, "expected": 0},
+            {"probs": np.array([0, 1]), "x": 0, "expected": -np.inf},
+        ]
+        for case in test_cases:
+            logprob = Categorical(case["probs"]).logpdf(case["x"])
+            self.assertEqual(logprob, case["expected"])
+
+    def test_logpdf_sanity_checks(self):
+        test_cases = []
+        for case in test_cases:
+            logprob = Categorical(case["probs"]).logpdf(case["x"])
+            self.assertEqual(logprob, case["expected"])
+
+    #
+    # SAMPLING SHAPE
+    #
+
+    def test_sample_single_parameter(self):
+        test_cases = [
+            {"sample_shape": (), "expected_shape": (1,)},
+            {"sample_shape": (100,), "expected_shape": (100, 1)},
+            {"sample_shape": (100, 10), "expected_shape": (100, 10, 1)},
+            {"sample_shape": (1, 100), "expected_shape": (1, 100, 1)},
+        ]
+        for case in test_cases:
+            samples = Categorical(np.array([0.2, 0.7, 0.1])).sample(
+                self.rng_key, case["sample_shape"]
+            )
+            self.assertEqual(samples.shape, case["expected_shape"])
+
+    def test_sample_array_of_parameters(self):
+        test_cases = [
+            {
+                "probs": np.array([[0.1, 0.9], [0.2, 0.3], [0.5, 0.5]]),
+                "sample_shape": (),
+                "expected_shape": (3,),
+            },
+            {
+                "probs": np.array([[0.1, 0.9], [0.2, 0.3], [0.5, 0.5]]),
+                "sample_shape": (100,),
+                "expected_shape": (100, 3),
+            },
+            {
+                "probs": np.array([[0.1, 0.9], [0.2, 0.3], [0.5, 0.5]]),
+                "sample_shape": (100, 10),
+                "expected_shape": (100, 10, 3),
+            },
+            {
+                "probs": np.array(
+                    [[[0.1, 0.9], [0.2, 0.3], [0.5, 0.5]], [[0.1, 0.9], [0.2, 0.3], [0.5, 0.5]]]
+                ),
+                "sample_shape": (100, 10),
+                "expected_shape": (100, 10, 2, 3),
+            },
+        ]
+        for case in test_cases:
+            samples = Categorical(case["probs"]).sample(self.rng_key, case["sample_shape"])
+            self.assertEqual(samples.shape, case["expected_shape"])
+
+    #
+    # LOGPDF SHAPES
+    #
+
+    def test_logpdf_shape(self):
+        test_cases = [
+            {"x": 0, "probs": np.array([0.4, 0.1, 0.5]), "expected_shape": ()},
+            {"x": 1, "probs": np.array([[0.1, 0.9], [0.2, 0.8]]), "expected_shape": (2,)},
+        ]
+        for case in test_cases:
+            log_prob = Categorical(case["probs"]).logpdf(case["x"])
+            self.assertEqual(log_prob.shape, case["expected_shape"])
+
+
+if __name__ == "__main__":
+    unittest.main()
