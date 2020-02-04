cd361a853b1641842ad67825381674fb71a952d5
Rémi Louf <remilouf@gmail.com>
2020-02-04 09:24:27 +0100

    Add draft for Bayesian layers
    We add a first draft for Bayesian layers. We would like to build upon
    the great work that went into the `trax` library by subclassing its
    layers and constructs, turning it into a distribution by adding `logpdf`
    and `sample` methods.

---
+++ mcx/layers/core.py
@@ -0,0 +1,43 @@
+from typing import Callable, Optional
+
+import jax.numpy as np
+
+import mcx.distributions as md
+import mcx.layers as ml
+import trax.layers as tl
+
+
+class Dense(tl.Dense, md.Distribution):
+    """Dense layer which basically computes:
+
+    ..math: `X * w + b`
+    """
+
+    def __init__(
+        self,
+        n_units: int,
+        distribution: Optional[md.Distribution] = None,
+        fn: Optional[Callable] = None,
+        kernel_initializer=tl.init.GlorotUniformInitializer(),
+        bias_initializer=tl.init.RandomNormalInitializer(1e-6),
+    ):
+        super(tl.Dense, self).__init__(n_units, kernel_initializer, bias_initializer)
+        self.distribution = distribution if distribution else md.Deterministic
+        self.fn = fn if fn else lambda x: x
+
+    def forward(self, x, weights):
+        w, b = weights
+        w_tilde = self.transform(w)
+        return np.dot(x, w_tilde) + b
+
+    def sample(self, x, weights, rng_key, sample_shape):
+        """Used during forward sample"""
+        w, b = weights
+        w_tilde = self.distribution.sample(
+            rng_key, sample_shape
+        )  # this may be problematic
+        return np.dot(x, w_tilde) + b
+
+    def logpdf(self, weights):
+        w, b = weights
+        return self.distribution.logpdf(w)
