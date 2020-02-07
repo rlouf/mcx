21d95be804a2514267206c14632d4114945cff47
Rémi Louf <remilouf@gmail.com>
2020-02-07 16:38:23 +0100

    add exponential distribution

---
+++ mcx/distributions/exponential.py
@@ -0,0 +1,25 @@
+from jax import lax, random
+from jax.scipy import stats
+
+from . import constraints
+from .distribution import Distribution
+from .utils import limit_to_support
+
+
+class Exponential(Distribution):
+    parameters = {"lmbda": constraints.strictly_positive}
+    support = constraints.positive
+
+    def __init__(self, lmbda):
+        self.event_shape = ()
+        self.batch_shape = lmbda.shape
+        self.lmbda = lmbda
+
+    def sample(self, rng_key, sample_shape=()):
+        shape = sample_shape + self.event_shape + self.batch_shape
+        return random.exponential(rng_key, shape=shape) / self.lmbda
+
+    @limit_to_support
+    def logpdf(self, x):
+        scale = lax.div(1.0, self.lmbda)
+        return stats.exponential(x, loc=0.0, scale=scale)
