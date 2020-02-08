Fix the batch shape broadcasting
We currently have the parameters directly as arguments of
`broadcast_batch_shape`. This works fine in most cases, but in the
case of the categorical, for instance, the batch shape is a subset of
the parameter's total shape.

We thus modify the function to take as input the shapes that we pass to
it. Fixes issues with the tests on shape for the categorical.

mcx/distributions/beta.py
@@ -1,3 +1,4 @@
+from jax import numpy as np
 from jax import random
 from jax.scipy import stats
 
@@ -15,7 +16,7 @@ class Beta(Distribution):
 
     def __init__(self, a, b):
         self.event_shape = ()
-        self.batch_shape = broadcast_batch_shape(a, b)
+        self.batch_shape = broadcast_batch_shape(np.shape(a), np.shape(b))
         self.a = a
         self.b = b
 

mcx/distributions/binomial.py
@@ -20,7 +20,7 @@ class Binomial(Distribution):
         self.support = constraints.integer_interval(0, n)
 
         self.event_shape = ()
-        self.batch_shape = broadcast_batch_shape(p, n)
+        self.batch_shape = broadcast_batch_shape(np.shape(p), np.shape(n))
         self.n = n
         self.p = p
 

mcx/distributions/discrete_uniform.py
@@ -15,7 +15,7 @@ class DiscreteUniform(Distribution):
         self.support = constraints.integer_interval(lower, upper)
 
         self.event_shape = ()
-        self.batch_shape = broadcast_batch_shape(lower, upper)
+        self.batch_shape = broadcast_batch_shape(np.shape(lower), np.shape(upper))
         self.lower = np.floor(lower)
         self.upper = np.floor(upper)
 

mcx/distributions/exponential.py
@@ -1,9 +1,10 @@
 from jax import lax, random
+from jax import numpy as np
 from jax.scipy import stats
 
 from . import constraints
 from .distribution import Distribution
-from .utils import limit_to_support
+from .utils import broadcast_batch_shape, limit_to_support
 
 
 class Exponential(Distribution):
@@ -12,7 +13,7 @@ class Exponential(Distribution):
 
     def __init__(self, lmbda):
         self.event_shape = ()
-        self.batch_shape = lmbda.shape
+        self.batch_shape = broadcast_batch_shape(np.shape(lmbda))
         self.lmbda = lmbda
 
     def sample(self, rng_key, sample_shape=()):

mcx/distributions/lognormal.py
@@ -12,7 +12,7 @@ class LogNormal(Distribution):
 
     def __init__(self, mu, sigma):
         self.event_shape = ()
-        self.batch_shape = broadcast_batch_shape(mu, sigma)
+        self.batch_shape = broadcast_batch_shape(np.shape(mu), np.shape(sigma))
         self.mu = mu
         self.sigma = sigma
 

mcx/distributions/normal.py
@@ -1,3 +1,4 @@
+from jax import numpy as np
 from jax import random, scipy
 
 from . import constraints
@@ -14,7 +15,7 @@ class Normal(Distribution):
 
     def __init__(self, mu, sigma):
         self.event_shape = ()
-        self.batch_shape = broadcast_batch_shape(mu, sigma)
+        self.batch_shape = broadcast_batch_shape(np.shape(mu), np.shape(sigma))
         self.mu = mu
         self.sigma = sigma
         super(Normal, self).__init__()

mcx/distributions/poisson.py
@@ -17,7 +17,7 @@ class Poisson(Distribution):
 
     def __init__(self, lmbda):
         self.event_shape = ()
-        self.batch_shape = broadcast_batch_shape(lmbda)
+        self.batch_shape = broadcast_batch_shape(np.shape(lmbda))
         self.lmbda = lmbda
 
     def sample(self, rng_key, sample_shape):

mcx/distributions/uniform.py
@@ -13,7 +13,7 @@ class Uniform(Distribution):
         self.support = constraints.closed_interval(lower, upper)
 
         self.event_shape = ()
-        self.batch_shape = broadcast_batch_shape(lower, upper)
+        self.batch_shape = broadcast_batch_shape(np.shape(lower), np.shape(upper))
         self.lower = lower
         self.upper = upper
 

mcx/distributions/utils.py
@@ -2,7 +2,7 @@ from jax import lax
 from jax import numpy as np
 
 
-def broadcast_batch_shape(*args):
+def broadcast_batch_shape(*shapes):
     """Compute the batch shape by broadcasting the arguments.
 
     We use `lax.broadcast_shapes` to get the shape of broadcasted arguments. We
@@ -22,7 +22,6 @@ def broadcast_batch_shape(*args):
     the fact that a distribution initiated with scalar arguments has a batch
     size of 1.
     """
-    shapes = [np.shape(arg) for arg in args]
     broadcasted_shape = lax.broadcast_shapes(*shapes)
     if len(broadcasted_shape) == 0:
         return (1,)