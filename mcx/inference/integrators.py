91672c880e682d5cf934c5f33e176fc25eb01967
Rémi Louf <remilouf@gmail.com>
2020-02-10 10:13:16 +0100

    add type hint and clean integrators

--- mcx/inference/integrators.py
+++ mcx/inference/integrators.py
@@ -1,16 +1,30 @@
 """Integrators for Hamiltonian and Riemanian trajectories.
+
+.. note:
+    This file is a "flat zone": position and momentum are one-dimensional arrays.
+    Any raveling/unraveling logic must be placed at a higher level.
 """
 from functools import partial
+from typing import Callable, Tuple
 
 import jax
-from jax import numpy as np
+import jax.numpy as np
+import jax.numpy.DeviceArray as Array
 
 
-@partial(jax.jit, static_argnums=(4,))
+__all__ = ["leapfrog_integrator"]
+
+
+@partial(jax.jit, static_argnums=(0,))
 def leapfrog_integrator(
-    position, momentum, potential, potential_grad, path_length, step_size
-):
-    """Second order symplectic integrator that uses the leapfrog algorithm
+    potential: Callable,
+    path_length: float,
+    step_size: Array,
+    position: Array,
+    momentum: Array,
+    potential_grad: Array,
+) -> Tuple[Array, Array, Array, Array]:
+    """Second order symplectic integrator that uses the leapfrog algorithm.
     """
     position, momentum = np.copy(position), np.copy(momentum)
     momentum -= step_size * potential_grad / 2  # half step
