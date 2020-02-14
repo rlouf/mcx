93325397cbd2c7819f4129cfb162bba385b2ea6a
Rémi Louf <remilouf@gmail.com>
2020-02-14 17:07:21 +0100

    refactor the leapfrog integrator

--- mcx/inference/integrators.py
+++ mcx/inference/integrators.py
@@ -4,8 +4,7 @@
     This file is a "flat zone": position and momentum are one-dimensional arrays.
     Any raveling/unraveling logic must be placed at a higher level.
 """
-from functools import partial
-from typing import Callable, NamedTuple, Tuple
+from typing import Callable, Tuple
 
 import jax
 import jax.numpy as np
@@ -14,29 +13,27 @@ import jax.numpy.DeviceArray as Array
 
 __all__ = ["leapfrog_integrator"]
 
-
-class IntegratorState(NamedTuple):
-    position: Array
-    momentum: Array
-    log_prob: float
-    log_prob_grad: float
+IntegratorState = Tuple[Array, Array, float, float]
 
 
-@partial(jax.jit, static_argnums=(1,))
 def leapfrog_integrator(
-    state: IntegratorState, logpdf: Callable, path_length: float, step_size: float,
-) -> Tuple[Array, Array, float, float]:
-    """Second order symplectic integrator that uses the leapfrog algorithm.
-    """
-    position, momentum, _, log_prob_grad = state
+    logpdf: Callable, path_length: float, step_size: float,
+) -> Callable:
+    @jax.jit
+    def step(_, state: IntegratorState) -> IntegratorState:
+        """Second order symplectic integrator that uses the leapfrog algorithm.
+        """
+        position, momentum, _, log_prob_grad = state
 
-    momentum -= step_size * log_prob_grad / 2  # half step
-    for _ in np.arange(np.round(path_length / step_size) - 1):
+        momentum -= step_size * log_prob_grad / 2  # half step
+        for _ in np.arange(np.round(path_length / step_size) - 1):
+            position = position + step_size * momentum  # whole step
+            log_prob, log_prob_grad = logpdf(position)
+            momentum = momentum - step_size * log_prob_grad  # whole step
         position = position + step_size * momentum  # whole step
         log_prob, log_prob_grad = logpdf(position)
-        momentum = momentum - step_size * log_prob_grad  # whole step
-    position = position + step_size * momentum  # whole step
-    log_prob, log_prob_grad = logpdf(position)
-    momentum = momentum - step_size * log_prob_grad / 2  # half step
+        momentum = momentum - step_size * log_prob_grad / 2  # half step
 
-    return IntegratorState(position, momentum, log_prob, log_prob_grad)
+        return (position, momentum, log_prob, log_prob_grad)
+
+    return step
