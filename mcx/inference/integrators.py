c3ac97006e750f011dea5c41d807f52af707f7bc
Rémi Louf <remilouf@gmail.com>
2020-02-14 17:41:56 +0100

    separate integrator from integrator step

--- mcx/inference/integrators.py
+++ mcx/inference/integrators.py
@@ -4,23 +4,51 @@
     This file is a "flat zone": position and momentum are one-dimensional arrays.
     Any raveling/unraveling logic must be placed at a higher level.
 """
-from typing import Callable, Tuple
+from typing import Callable, NamedTuple
 
 import jax
 import jax.numpy as np
 import jax.numpy.DeviceArray as Array
 
 
-__all__ = ["leapfrog_integrator"]
-
-IntegratorState = Tuple[Array, Array, float, float]
+__all__ = ["hmc_integrator"]
 
 
-def leapfrog_integrator(
-    logpdf: Callable, path_length: float, step_size: float,
+class IntegratorState(NamedTuple):
+    position: Array
+    momentum: Array
+    log_prob: float
+    log_prob_grad: float
+
+
+def hmc_integrator(
+    integrator_step: Callable, path_length: float, step_size: float
 ) -> Callable:
     @jax.jit
-    def step(_, state: IntegratorState) -> IntegratorState:
+    def integrate(_, state: IntegratorState) -> IntegratorState:
+        state = integrator_step(state, step_size, path_length)
+        return state
+
+    return integrate
+
+
+def ehmc_integrator(
+    integrator_step: Callable, path_length_generator: Callable, step_size: float
+) -> Callable:
+    @jax.jit
+    def integrate(rng_key, state: IntegratorState) -> IntegratorState:
+        path_length = path_length_generator(rng_key)
+        state = integrator_step(state, step_size, path_length)
+        return state
+
+    return integrate
+
+
+def leapfrog(logpdf: Callable) -> Callable:
+    @jax.jit
+    def step(
+        state: IntegratorState, step_size: float, path_length: float
+    ) -> IntegratorState:
         """Second order symplectic integrator that uses the leapfrog algorithm.
         """
         position, momentum, _, log_prob_grad = state
@@ -34,6 +62,6 @@ def leapfrog_integrator(
         log_prob, log_prob_grad = logpdf(position)
         momentum = momentum - step_size * log_prob_grad / 2  # half step
 
-        return (position, momentum, log_prob, log_prob_grad)
+        return IntegratorState(position, momentum, log_prob, log_prob_grad)
 
     return step
