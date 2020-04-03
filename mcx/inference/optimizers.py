bdd4e189d91902b0ac8ab1b3dd2718acf5174e14
Rémi Louf <remilouf@gmail.com>
2020-04-03 15:27:54 +0200

    s/weights/position in optimizers

--- mcx/inference/optimizers.py
+++ mcx/inference/optimizers.py
@@ -18,13 +18,13 @@ def Adam(
     b2: float = 0.999,
     eps: float = 1e-5,
 ) -> Tuple[Callable, Callable]:
-    def init(weights: np.DeviceArray) -> AdamState:
-        m = np.zeros_like(weights)
-        v = np.zeros_like(weights)
+    def init(position: np.DeviceArray) -> AdamState:
+        m = np.zeros_like(position)
+        v = np.zeros_like(position)
         return AdamState(m, v, 0)
 
     def update(
-        state: AdamState, weights: np.DeviceArray, gradients: np.DeviceArray
+        state: AdamState, position: np.DeviceArray, gradients: np.DeviceArray
     ) -> Tuple[np.DeviceArray, AdamState]:
         m, v, step = state
 
@@ -33,11 +33,11 @@ def Adam(
 
         mhat = m / (1 - b1 ** (step + 1))
         vhat = v / (1 - b2 ** (step + 1))
-        new_weights = (1 - weight_decay_rate) * weights - (
+        new_position = (1 - weight_decay_rate) * position - (
             learning_rate * mhat / (np.sqrt(vhat) + eps)
-        ).astype(weights.dtype)
+        ).astype(position.dtype)
 
-        return new_weights, AdamState(m, v, step + 1)
+        return new_position, AdamState(m, v, step + 1)
 
     return init, update
 
@@ -47,20 +47,20 @@ class RMSPropState(NamedTuple):
 
 
 def RMSProp(learning_rate: float = 0.001, gamma: float = 0.9, eps: float = 1e-5):
-    def init(weights: np.DeviceArray) -> RMSPropState:
-        return RMSPropState(np.ones_like(weights))
+    def init(position: np.DeviceArray) -> RMSPropState:
+        return RMSPropState(np.ones_like(position))
 
     def update(
-        state: RMSPropState, weights: np.DeviceArray, gradients: np.DeviceArray
+        state: RMSPropState, position: np.DeviceArray, gradients: np.DeviceArray
     ) -> Tuple[np.DeviceArray, RMSPropState]:
         avg_squared_gradients = state[0]
         avg_squared_gradients = avg_squared_gradients * gamma + gradients ** 2 * (
             1.0 - gamma
         )
-        new_weights = weights - (
+        new_position = position - (
             learning_rate * gradients / (np.sqrt(avg_squared_gradients) + eps)
-        ).astype(weights.dtype)
+        ).astype(position.dtype)
 
-        return new_weights, RMSPropState(avg_squared_gradients)
+        return new_position, RMSPropState(avg_squared_gradients)
 
     return init, update
