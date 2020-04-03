825fa46dad8b1e23c10dcfc5001510da64d02562
Rémi Louf <remilouf@gmail.com>
2020-04-03 15:26:35 +0200

    add the RMSProp optimizer

--- mcx/inference/optimizers.py
+++ mcx/inference/optimizers.py
@@ -24,12 +24,12 @@ def Adam(
         return AdamState(m, v, 0)
 
     def update(
-        state: AdamState, weights: np.DeviceArray, grads: np.DeviceArray
+        state: AdamState, weights: np.DeviceArray, gradients: np.DeviceArray
     ) -> Tuple[np.DeviceArray, AdamState]:
         m, v, step = state
 
-        m = (1 - b1) * grads + b1
-        v = (1 - b2) * (grads ** 2) + b2
+        m = (1 - b1) * gradients + b1
+        v = (1 - b2) * (gradients ** 2) + b2
 
         mhat = m / (1 - b1 ** (step + 1))
         vhat = v / (1 - b2 ** (step + 1))
@@ -38,5 +38,29 @@ def Adam(
         ).astype(weights.dtype)
 
         return new_weights, AdamState(m, v, step + 1)
+
+    return init, update
+
+
+class RMSPropState(NamedTuple):
+    avg_squared_gradients: float
+
+
+def RMSProp(learning_rate: float = 0.001, gamma: float = 0.9, eps: float = 1e-5):
+    def init(weights: np.DeviceArray) -> RMSPropState:
+        return RMSPropState(np.ones_like(weights))
+
+    def update(
+        state: RMSPropState, weights: np.DeviceArray, gradients: np.DeviceArray
+    ) -> Tuple[np.DeviceArray, RMSPropState]:
+        avg_squared_gradients = state[0]
+        avg_squared_gradients = avg_squared_gradients * gamma + gradients ** 2 * (
+            1.0 - gamma
+        )
+        new_weights = weights - (
+            learning_rate * gradients / (np.sqrt(avg_squared_gradients) + eps)
+        ).astype(weights.dtype)
+
+        return new_weights, RMSPropState(avg_squared_gradients)
 
     return init, update
