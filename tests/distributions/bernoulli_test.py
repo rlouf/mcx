d59d686da85b2c2fef8c800b98d0e5696a6533b8
Rémi Louf <remilouf@gmail.com>
2020-05-06 09:57:20 +0200

    test the Bernoulli distribution with pytest

--- tests/distributions/bernoulli_test.py
+++ tests/distributions/bernoulli_test.py
@@ -1,141 +1,154 @@
-import unittest
-
 import jax
 from jax import numpy as np
-from numpy.testing import assert_array_almost_equal
+import numpy as onp
+import pytest
 
 from mcx.distributions import Bernoulli
 
 
-class BernoulliTest(unittest.TestCase):
-    def setUp(self):
-        self.rng_key = jax.random.PRNGKey(0)
-
-    #
-    # SAMPLING CORRECTNESS
-    #
-
-    def test_sample_frequency(self):
-        probabilities = [0, 1, 0.5]
-        for p in probabilities:
-            samples = Bernoulli(p).sample(self.rng_key, (1_000_000,))
-            avg = np.mean(samples, axis=0).item()
-            self.assertAlmostEqual(avg, p, places=3)
-
-    def test_sample_frequency_vectorized(self):
-        probabilities = np.array([0, 1, 0.5])
-        samples = Bernoulli(probabilities).sample(self.rng_key, (1_000_000,))
-        avg = np.mean(samples, axis=0).__array__()
-        assert_array_almost_equal(probabilities, avg, decimal=3)
-
-    #
-    # LOGPDF CORRECTNESS
-    #
-
-    def test_logpdf_edge_cases(self):
-        test_cases = [
-            {"p": 1, "x": 0, "expected": -np.inf},
-            {"p": 0, "x": 1, "expected": -np.inf},
-        ]
-        for case in test_cases:
-            logprob = Bernoulli(case["p"]).logpdf(case["x"])
-            self.assertEqual(logprob.item(), case["expected"])
-
-    def test_logpdf_out_of_support(self):
-        test_cases = [
-            {"p": 0.5, "x": 0.5, "expected": -np.inf},
-            {"p": 0.5, "x": 3, "expected": -np.inf},
-            {"p": 0.5, "x": -1.1, "expected": -np.inf},
-        ]
-        for case in test_cases:
-            logprob = Bernoulli(case["p"]).logpdf(case["x"])
-            self.assertEqual(logprob.item(), case["expected"])
-
-    def test_logpdf_few_cases(self):
-        """Values obtained from `scipy.stats.bernoulli.logpmf`
-
-        Note:
-            Comparison fails at the 8th decimal place.
-        """
-        test_cases = [
-            {"p": 0.5, "x": 0, "expected": -0.6931471805599453},
-            {"p": 0.5, "x": 1, "expected": -0.6931471805599453},
-            {"p": 0.2, "x": 1, "expected": -1.6094379124341003},
-            {"p": 0.2, "x": 0, "expected": -0.22314355131420976},
-            {"p": 0.7, "x": 1, "expected": -0.35667494393873245},
-            {"p": 0.7, "x": 0, "expected": -1.203972804325936},
-        ]
-        for case in test_cases:
-            logprob = Bernoulli(case["p"]).logpdf(case["x"])
-            self.assertAlmostEqual(logprob.item(), case["expected"], places=7)
-
-    #
-    # SAMPLING SHAPES
-    #
-
-    def test_sample_shape_scalar_arguments(self):
-        """Test the correctness of broadcasting when both arguments are
-        scalars. They are tested separately as scalars are an edge case when
-        it comes to broadcasting.
-
-        The trailing `1` stands for the batch size.
-        """
-        test_cases = [
-            {"sample_shape": (), "expected_shape": (1,)},
-            {"sample_shape": (100,), "expected_shape": (100, 1)},
-            {"sample_shape": (100, 10), "expected_shape": (100, 10, 1)},
-            {"sample_shape": (1, 100), "expected_shape": (1, 100, 1)},
-        ]
-        for case in test_cases:
-            samples = Bernoulli(0.5).sample(self.rng_key, case["sample_shape"])
-            self.assertEqual(samples.shape, case["expected_shape"])
-
-    def test_sample_shape_array_parameter(self):
-        test_cases = [
-            {"p": np.array([0.3, 0.5]), "sample_shape": (), "expected_shape": (2,)},
-            {
-                "p": np.array([[0.3, 0.5], [0.1, 0.2]]),
-                "sample_shape": (),
-                "expected_shape": (2, 2),
-            },
-            {
-                "p": np.array([0.3, 0.5]),
-                "sample_shape": (10,),
-                "expected_shape": (10, 2),
-            },
-            {
-                "p": np.array([[0.3, 0.5], [0.1, 0.2]]),
-                "sample_shape": (10,),
-                "expected_shape": (10, 2, 2),
-            },
-            {
-                "p": np.array([0.3, 0.5]),
-                "sample_shape": (10, 10),
-                "expected_shape": (10, 10, 2),
-            },
-            {
-                "p": np.array([[0.3, 0.5], [0.1, 0.2]]),
-                "sample_shape": (10, 10),
-                "expected_shape": (10, 10, 2, 2),
-            },
-        ]
-        for case in test_cases:
-            samples = Bernoulli(case["p"]).sample(self.rng_key, case["sample_shape"])
-            self.assertEqual(samples.shape, case["expected_shape"])
-
-    #
-    # LOGPDF SHAPES
-    #
-
-    def test_logpdf_shape(self):
-        test_cases = [
-            {"x": 1, "p": 0, "expected_shape": ()},
-            {"x": 1, "p": np.array([0.1, 0.2]), "expected_shape": (2,)},
-        ]
-        for case in test_cases:
-            log_prob = Bernoulli(case["p"]).logpdf(case["x"])
-            self.assertEqual(log_prob.shape, case["expected_shape"])
+@pytest.fixture
+def rng_key():
+    return jax.random.PRNGKey(0)
 
 
-if __name__ == "__main__":
-    unittest.main()
+#
+# SAMPLING CORRECTNESS
+#
+
+
+@pytest.mark.parametrize("p", [0, 1, 0.5])
+def test_sample_frequency(rng_key, p):
+    samples = Bernoulli(p).sample(rng_key, (1_000_000,))
+    avg = np.mean(samples, axis=0).item()
+    assert avg == pytest.approx(p, abs=1e-3)
+
+
+def test_sample_frequency_vectorized(rng_key):
+    probabilities = np.array([0, 1, 0.5])
+    samples = Bernoulli(probabilities).sample(rng_key, (1_000_000,))
+    averages = np.mean(samples, axis=0)
+
+    p_array = onp.asarray(probabilities)
+    avg_array = onp.asarray(averages)
+    assert avg_array == pytest.approx(p_array, abs=1e-3)
+
+
+#
+# LOGPDF CORRECTNESS
+#
+
+
+edge_cases = [
+    {"p": 1, "x": 0, "expected": -np.inf},
+    {"p": 0, "x": 1, "expected": -np.inf},
+]
+
+
+@pytest.mark.parametrize("case", edge_cases)
+def test_logpdf_edge_cases(rng_key, case):
+    logprob = Bernoulli(case["p"]).logpdf(case["x"])
+    assert logprob.item() == pytest.approx(case["expected"])
+
+
+out_of_support_cases = [
+    {"p": 0.5, "x": 0.5, "expected": -np.inf},
+    {"p": 0.5, "x": 3, "expected": -np.inf},
+    {"p": 0.5, "x": -1.1, "expected": -np.inf},
+]
+
+
+@pytest.mark.parametrize("case", out_of_support_cases)
+def test_logpdf_out_of_support(rng_key, case):
+    logprob = Bernoulli(case["p"]).logpdf(case["x"])
+    assert logprob.item() == case["expected"]
+
+
+example_values = [
+    {"p": 0.5, "x": 0, "expected": -0.6931471805599453},
+    {"p": 0.5, "x": 1, "expected": -0.6931471805599453},
+    {"p": 0.2, "x": 1, "expected": -1.6094379124341003},
+    {"p": 0.2, "x": 0, "expected": -0.22314355131420976},
+    {"p": 0.7, "x": 1, "expected": -0.35667494393873245},
+    {"p": 0.7, "x": 0, "expected": -1.203972804325936},
+]
+
+
+@pytest.mark.parametrize("example", example_values)
+def test_logpdf_example_values(rng_key, example):
+    """The values are obtained from `scipy.stats.bernoulli.logpmf`.
+    """
+    logprob = Bernoulli(example["p"]).logpdf(example["x"])
+    assert logprob.item() == pytest.approx(example["expected"], abs=1e-7)
+
+
+#
+# LOGPDF SHAPES
+#
+
+
+expected_logpdf_shapes = [
+    {"x": 1, "p": 0, "expected_shape": ()},
+    {"x": 1, "p": np.array([0.1, 0.2]), "expected_shape": (2,)},
+]
+
+
+@pytest.mark.parametrize("case", expected_logpdf_shapes)
+def test_logpdf_shape(rng_key, case):
+    log_prob = Bernoulli(case["p"]).logpdf(case["x"])
+    assert log_prob.shape == case["expected_shape"]
+
+
+#
+# SAMPLING SHAPES
+#
+
+expected_sample_shapes = [
+    {"sample_shape": (), "expected_shape": (1,)},
+    {"sample_shape": (100,), "expected_shape": (100, 1)},
+    {"sample_shape": (100, 10), "expected_shape": (100, 10, 1)},
+    {"sample_shape": (1, 100), "expected_shape": (1, 100, 1)},
+]
+
+
+@pytest.mark.parametrize("case", expected_sample_shapes)
+def test_sample_shape_scalar_arguments(rng_key, case):
+    """Test the correctness of broadcasting when both arguments are
+    scalars. We test scalars arguments separately from array arguments
+    since scalars are edge cases when it comes to broadcasting.
+
+    The trailing `1` in the result shapes stands for the batch size.
+    """
+    samples = Bernoulli(0.5).sample(rng_key, case["sample_shape"])
+    assert samples.shape == case["expected_shape"]
+
+
+expected_sample_shapes = [
+    {"p": np.array([0.3, 0.5]), "sample_shape": (), "expected_shape": (2,)},
+    {
+        "p": np.array([[0.3, 0.5], [0.1, 0.2]]),
+        "sample_shape": (),
+        "expected_shape": (2, 2),
+    },
+    {"p": np.array([0.3, 0.5]), "sample_shape": (10,), "expected_shape": (10, 2)},
+    {
+        "p": np.array([[0.3, 0.5], [0.1, 0.2]]),
+        "sample_shape": (10,),
+        "expected_shape": (10, 2, 2),
+    },
+    {
+        "p": np.array([0.3, 0.5]),
+        "sample_shape": (10, 10),
+        "expected_shape": (10, 10, 2),
+    },
+    {
+        "p": np.array([[0.3, 0.5], [0.1, 0.2]]),
+        "sample_shape": (10, 10),
+        "expected_shape": (10, 10, 2, 2),
+    },
+]
+
+
+@pytest.mark.parametrize("case", expected_sample_shapes)
+def test_sample_shape_array_parameter(rng_key, case):
+    samples = Bernoulli(case["p"]).sample(rng_key, case["sample_shape"])
+    assert samples.shape == case["expected_shape"]
