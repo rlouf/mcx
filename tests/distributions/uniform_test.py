0b39f825444f95fa54b20a35daaf1ef481e07cb8
Rémi Louf <remilouf@gmail.com>
2020-05-07 10:53:27 +0200

    uniform tests to pytest

--- tests/distributions/uniform_test.py
+++ tests/distributions/uniform_test.py
@@ -1,42 +1,37 @@
-import unittest
-
 from jax import numpy as np
-from jax import random
+import pytest
 
 from mcx.distributions import Uniform
 
 
-class UniformTest(unittest.TestCase):
-    def setUp(self):
-        self.rng_key = random.PRNGKey(0)
-
-    #
-    # SAMPLING CORRECTNESS
-    #
-
-    #
-    # LOGPDF CORRECTNESS
-    #
-
-    def test_logpdf_out_of_support(self):
-        test_cases = [
-            {"x": 0, "expected": 0.0},  # boundary belongs to support
-            {"x": 1, "expected": 0.0},  # boundary belongs to support
-            {"x": -0.01, "expected": -np.inf},
-            {"x": 1.001, "expected": -np.inf},
-        ]
-        for case in test_cases:
-            logprob = Uniform(0, 1).logpdf(case["x"])
-            self.assertEqual(logprob, case["expected"])
-
-    #
-    # SAMPLING SHAPE
-    #
-
-    #
-    # LOGPDF SHAPES
-    #
+#
+# SAMPLING CORRECTNESS
+#
 
 
-if __name__ == "__main__":
-    unittest.main()
+#
+# LOGPDF CORRECTNESS
+#
+
+out_of_support_cases = [
+    {"x": 0, "expected": 0.0},  # boundary belongs to support
+    {"x": 1, "expected": 0.0},  # boundary belongs to support
+    {"x": -0.01, "expected": -np.inf},
+    {"x": 1.001, "expected": -np.inf},
+]
+
+
+@pytest.mark.parametrize("case", out_of_support_cases)
+def test_logpdf_out_of_support(case):
+    logprob = Uniform(0, 1).logpdf(case["x"])
+    assert logprob == case["expected"]
+
+
+#
+# SAMPLING SHAPE
+#
+
+
+#
+# LOGPDF SHAPES
+#
