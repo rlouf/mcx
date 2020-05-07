311ed675a74e5b231c059e165dfc4689e93b1948
Rémi Louf <remilouf@gmail.com>
2020-05-07 10:56:23 +0200

    poisson tests to pytest

--- tests/distributions/poisson_test.py
+++ tests/distributions/poisson_test.py
@@ -1,41 +1,35 @@
-import unittest
-
 from jax import numpy as np
-from jax import random
+import pytest
 
 from mcx.distributions import Poisson
 
 
-class PoissonTest(unittest.TestCase):
-    def setUp(self):
-        self.rng_key = random.PRNGKey(0)
-
-    #
-    # SAMPLING CORRECTNESS
-    #
-
-    #
-    # LOGPDF CORRECTNESS
-    #
-
-    def test_logpdf_edge_cases(self):
-        # Should work with ints
-        pass
-
-    def test_logpdf_out_of_support(self):
-        test_cases = [{"x": -1.0, "expected": -np.inf}, {"x": 1.1, "expected": -np.inf}]
-        for case in test_cases:
-            logprob = Poisson(1.0).logpdf(case["x"])
-            self.assertEqual(logprob, case["expected"])
-
-    #
-    # SAMPLING SHAPE
-    #
-
-    #
-    # LOGPDF SHAPES
-    #
+#
+# SAMPLING CORRECTNESS
+#
 
 
-if __name__ == "__main__":
-    unittest.main()
+#
+# LOGPDF CORRECTNESS
+#
+
+out_of_support_cases = [
+    {"x": -1.0, "expected": -np.inf},
+    {"x": 1.1, "expected": -np.inf},
+]
+
+
+@pytest.mark.parametrize("case", out_of_support_cases)
+def test_logpdf_out_of_support(case):
+    logprob = Poisson(1.0).logpdf(case["x"])
+    assert logprob == case["expected"]
+
+
+#
+# SAMPLING SHAPE
+#
+
+
+#
+# LOGPDF SHAPES
+#
