be502fb044b4821723124429cc370f22d311a008
Rémi Louf <remilouf@gmail.com>
2020-05-07 10:58:54 +0200

    lognormal tests to pytest

--- tests/distributions/lognormal_test.py
+++ tests/distributions/lognormal_test.py
@@ -1,37 +1,32 @@
-import unittest
-
 from jax import numpy as np
-from jax import random
+import pytest
 
 from mcx.distributions import LogNormal
 
 
-class LogNormalTest(unittest.TestCase):
-    def setUp(self):
-        self.rng_key = random.PRNGKey(0)
-
-    #
-    # SAMPLING CORRECTNESS
-    #
-
-    #
-    # LOGPDF CORRECTNESS
-    #
-
-    def test_out_of_support(self):
-        test_cases = [{"x": 0, "expected": -np.inf}, {"x": -1, "expected": -np.inf}]
-        for case in test_cases:
-            logprob = LogNormal(0, 1).logpdf(case["x"])
-            self.assertEqual(logprob, case["expected"])
-
-    #
-    # SAMPLING SHAPE
-    #
-
-    #
-    # LOGPDF SHAPES
-    #
+#
+# SAMPLING CORRECTNESS
+#
 
 
-if __name__ == "__main__":
-    unittest.main()
+#
+# LOGPDF CORRECTNESS
+#
+
+out_of_support_cases = [{"x": 0, "expected": -np.inf}, {"x": -1, "expected": -np.inf}]
+
+
+@pytest.mark.parametrize("case", out_of_support_cases)
+def test_out_of_support(case):
+    logprob = LogNormal(0, 1).logpdf(case["x"])
+    assert logprob == case["expected"]
+
+
+#
+# SAMPLING SHAPE
+#
+
+
+#
+# LOGPDF SHAPES
+#
