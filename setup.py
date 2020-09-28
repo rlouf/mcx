90aa1f021dc3755cef41bef2ed5f3a7151aaf4f3
Rémi Louf <remilouf@gmail.com>
2020-09-28 17:19:05 +0200

    add Arviz to the library's dependencies

--- setup.py
+++ setup.py
@@ -12,5 +12,13 @@ setuptools.setup(
     licence="Apache Licence Version 2.0",
     packages=["mcx"],
     python_requires=">=3.5",
-    install_requires=["astor", "jax==0.1.77", "jaxlib", "networkx", "numpy", "tqdm"],
+    install_requires=[
+        "arviz",
+        "astor",
+        "jax==0.1.77",
+        "jaxlib",
+        "networkx",
+        "numpy",
+        "tqdm",
+    ],
 )
