b95fa5e990cd387bba90b837c1d2d6aaba7c095e
Rémi Louf <remilouf@gmail.com>
2020-09-29 21:06:34 +0200

    make the base sampler an iterable, remove iterative_sampler
    Since batch and iterative sampling share the same API and philosophy it
    would be preferable to combine both execution models in the same object.
    We made the sampler object also behave as an iterator so samples can be
    fetched in an iterative way.

--- mcx/__init__.py
+++ mcx/__init__.py
@@ -1,9 +1,7 @@
 from mcx.model import model
 from mcx.model import sample_forward
-from mcx.sampling import batch_sampler, iterative_sampler, sequential_sampler
+from mcx.sampling import sampler, sequential_sampler
 from mcx.inference.hmc import HMC
-
-sampler = batch_sampler
 
 __all__ = [
     "model",
@@ -11,6 +9,5 @@ __all__ = [
     "sample_forward",
     "HMC",
     "sampler",
-    "iterative_sampler",
     "sequential_sampler",
 ]
