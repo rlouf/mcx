86338d32b1b53eacf9864d066ad1d3dd9a32504c
Rémi Louf <remilouf@gmail.com>
2020-10-16 09:13:41 +0200

    remove sequential sampling dead code

--- mcx/__init__.py
+++ mcx/__init__.py
@@ -1,13 +1,12 @@
 from mcx.inference.hmc import HMC
 from mcx.model import model, sample_forward
-from mcx.sampling import sampler, sequential_sampler
+from mcx.sampling import sampler
 from mcx.trace import Trace
 
 __all__ = [
     "model",
     "sample_forward",
     "sampler",
-    "sequential_sampler",
     "HMC",
     "Trace",
 ]
