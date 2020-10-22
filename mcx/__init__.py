4f1a3f7b831daf249b65221087c3d150d6dc4eb8
Rémi Louf <remilouf@gmail.com>
2020-10-22 21:32:46 +0200

    rename sampling to sample

--- mcx/__init__.py
+++ mcx/__init__.py
@@ -1,6 +1,6 @@
 from mcx.inference.hmc import HMC
 from mcx.model import forward_sampler, model, predict
-from mcx.sampling import sampler
+from mcx.sample import sampler
 from mcx.trace import Trace
 
 __all__ = [
