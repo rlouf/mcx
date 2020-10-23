3a397f7cad0f5b0120710afbb8544db0b5cbd82d
Rémi Louf <remilouf@gmail.com>
2020-10-23 15:25:05 +0200

    add posterior predictive sampling

--- mcx/core/compiler.py
+++ mcx/core/compiler.py
@@ -470,3 +470,153 @@ def compile_to_prior_sampler(graph, namespace, jit=False) -> Artifact:
         fn_name,
         astor.code_gen.to_source(sampler_ast),
     )
+
+
+def compile_to_posterior_sampler(graph, namespace, jit=False) -> Artifact:
+    """Compile the model in a function that generates one posterior predictive
+    samples from the model's.
+
+    Example
+    -------
+
+        >>> @mcx.model
+        ... def linear_regression(x):
+        ...     sigma <~ Exponential(1.)
+        ...     weights <~ Normal(0, 1)
+        ...     z = np.dot(X, weights)
+        ...     y <~ Normal(z, sigma)
+        ...     return y
+
+    Should become:
+
+        >>> @mcx.model
+        ... def linear_regression(rng_key, x, sigma, weights):
+        ...     z = np.dot(X, weights)
+        ...     y = Normal(z, sigma).sample(rng_key)
+        ...     return y
+
+    Args:
+        model: A probabilistic program definition.
+        namespace: The model definition's global scope.
+    Returns:
+        sample_fn: A JIT compiled function that returns prior predictive
+            samples from the model. The function's signature is of the form:
+            `model_sampler(rng_key, *args, sample_shape=())`
+    """
+    fn_name = graph.name + "_posterior_sampler"
+
+    args = [
+        node[1]["content"].to_sampler()
+        for node in graph.nodes(data=True)
+        if isinstance(node[1]["content"], Argument)
+        and node[1]["content"].default_value is None
+    ]
+
+    kwargs = [
+        node[1]["content"]
+        for node in graph.nodes(data=True)
+        if isinstance(node[1]["content"], Argument)
+        and node[1]["content"].default_value is not None
+    ]
+
+    # Build the list of variables for which we will have posterior samples.
+    # Essentially all random_variables except the returned ones.
+    sampled_vars = [
+        ast.arg(arg=node[1]["content"].name, annotation=None)
+        for node in graph.nodes(data=True)
+        if isinstance(node[1]["content"], RandVar)
+        and node[1]["content"].is_returned is False
+    ]
+
+    fn_args = (
+        [ast.arg(arg="rng_key", annotation=None)]
+        + args
+        + sampled_vars
+        + [k.to_sampler() for k in kwargs]
+    )
+
+    fn_defaults = [k.default_value for k in kwargs]
+
+    # Simplify the graph.
+    #
+    # Not all variables sampled are necessary for predictions. We thus
+    # simplify the graph to avoid unnecessary computations. Note that we
+    # still take these variables as arguments to ease the predictor's life.
+    #
+    # We proceed by removing every random variable that is not returned
+    # from the graph and any deterministic computation in between.
+
+    pruned_graph = graph.copy()
+    for node in nx.topological_sort(graph):
+        var = graph.nodes[node]["content"]
+        if isinstance(var, RandVar) and not var.is_returned:
+            pruned_graph.remove_node(node)
+
+    connected_components = nx.algorithms.weakly_connected_components(pruned_graph)
+    nodes_to_keep = []
+    for component in connected_components:
+        has_returned = sum(
+            [pruned_graph.nodes[node]["content"].is_returned for node in component]
+        )
+        if has_returned:
+            nodes_to_keep += [node for node in component]
+
+    pruned_graph = nx.subgraph(pruned_graph, nodes_to_keep)
+
+    # Now add the variables to the function's body
+    body = []
+    ordered_nodes = [
+        pruned_graph.nodes[node]["content"]
+        for node in nx.topological_sort(pruned_graph)
+        if not isinstance(pruned_graph.nodes[node]["content"], Argument)
+    ]
+    for node in ordered_nodes:
+        body.append(node.to_sampler(graph))
+
+    returned_vars = [
+        ast.Name(id=node.name, ctx=ast.Load())
+        for node in ordered_nodes
+        if not isinstance(node, mcx.core.graph.Var) and node.is_returned
+    ]
+    if len(returned_vars) == 1:
+        returned = ast.Return(returned_vars[0])
+    else:
+        returned = ast.Return(
+            value=ast.Tuple(
+                elts=returned_vars,
+                ctx=ast.Load(),
+            )
+        )
+    body.append(returned)
+    sampler_ast = ast.Module(
+        body=[
+            ast.FunctionDef(
+                name=fn_name,
+                args=ast.arguments(
+                    args=fn_args,
+                    vararg=None,
+                    kwarg=None,
+                    posonlyargs=[],
+                    kwonlyargs=[],
+                    defaults=fn_defaults,
+                    kw_defaults=[],
+                ),
+                body=body,
+                decorator_list=[],
+            )
+        ],
+        type_ignores=[],
+    )
+    sampler_ast = ast.fix_missing_locations(sampler_ast)
+
+    sampler = compile(sampler_ast, filename="<ast>", mode="exec")
+    exec(sampler, namespace)
+
+    returned_names = [arg.id for arg in returned_vars]
+
+    return Artifact(
+        namespace[fn_name],
+        returned_names,
+        fn_name,
+        astor.code_gen.to_source(sampler_ast),
+    )
