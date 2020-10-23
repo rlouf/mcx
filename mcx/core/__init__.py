b8655dff1fd32eb91d176e26747b47baeff120bc
Rémi Louf <remilouf@gmail.com>
2020-10-23 15:25:05 +0200

    add posterior predictive sampling

--- mcx/core/__init__.py
+++ mcx/core/__init__.py
@@ -1,6 +1,7 @@
 from .compiler import (
     compile_to_loglikelihoods,
     compile_to_logpdf,
+    compile_to_posterior_sampler,
     compile_to_prior_sampler,
     compile_to_sampler,
 )
@@ -11,6 +12,7 @@ __all__ = [
     "compile_to_prior_sampler",
     "compile_to_logpdf",
     "compile_to_loglikelihoods",
+    "compile_to_posterior_sampler",
     "compile_to_sampler",
     "GraphicalModel",
     "parse_definition",
