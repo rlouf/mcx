5d5aa87e802dcf3a135abb4a7b60d48e0c19fe5f
Rémi Louf <remilouf@gmail.com>
2020-10-28 11:41:35 +0100

    move forward sampler to predict

--- mcx/__init__.py
+++ mcx/__init__.py
@@ -1,14 +1,14 @@
 from mcx.inference.hmc import HMC
-from mcx.model import forward_sampler, model
-from mcx.predict import predict
+from mcx.model import model
+from mcx.predict import predict, sample_forward
 from mcx.sample import sampler
 from mcx.trace import Trace
 
 __all__ = [
     "model",
-    "forward_sampler",
     "sampler",
     "HMC",
     "Trace",
     "predict",
+    "sample_forward",
 ]
