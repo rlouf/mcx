a7873ecb594a6bf2670a400cbc95c4c8e5a7a901
Rémi Louf <remilouf@gmail.com>
2020-11-11 21:32:27 +0100

    make rwmh warmup follow hmc warmup's design

--- mcx/inference/warmup/random_walk.py
+++ mcx/inference/warmup/random_walk.py
@@ -10,29 +10,48 @@ from mcx.inference.optimizers import Adam, AdamState
 class RWMHWarmupState(NamedTuple):
     parameter: np.DeviceArray
     adam_state: AdamState
+    acceptance_rate: float  # running acceptance rate
+    step: int
 
 
-def rwmh_warmup(kernel_factory, target_acceptance_rate):
-    """How do we make it such that the different values are
-    updated independently? All depends on the gradient. Needs
-    to be computed per variable?
+def rwmh_warmup(kernel_factory, target_acceptance_rate, **adam_args):
+    """Random Walk Metropolis Hastings warmup.
+
+    We use the Adam optimizer pour minimize the difference between the running
+    acceptance rate and the target acceptance rate. The running acceptance rate
+    is computed as the average number of times moves were chosen during the
+    acceptance step.
+
+    Parameters
+    ----------
+    kernel_factory
+        A function that returns a transition kernel given a value of the parameter.
+    target_acceptance_rate
+        The acceptance rate that we wish to attain by varying the value of the
+        parameter.
+    **adam_args
+        Arguments used to instantiate the Adam optimizer.
+
     """
+    adam_init, adam_update = Adam(**adam_args)
 
-    def init():
-        pass
+    def init(initial_parameter):
+        adam_state = adam_init(initial_parameter)
+        return RWMHWarmupState(initial_parameter, adam_state, 0.0, 0)
 
     @jax.jit
-    def update():
-        pass
+    def update(chain_state, warmup_state):
+        parameter, adam_state, acceptance_rate, step = warmup_state
+        kernel = kernel_factory(parameter)
+        new_chain_state, info = kernel(chain_state)
 
-    adam_init, adam_update = Adam(0.01)
-    adam_state = adam_init(sigma)
+        new_acceptance_rate = (step * acceptance_rate + info.is_accepted) / (step + 1)
+        gradient = target_acceptance_rate - new_acceptance_rate
+        new_parameter, new_adam_state = adam_update(adam_state, parameter, gradient)
 
-    kernel = kernel_factory(sigma)
-    state, info = kernel(state)
-    is_accepted = info.is_accepted
-    acceptance_rate = None  # Maybe an exponential average
+        return new_chain_state, RWMHWarmupState(
+            new_parameter, new_adam_state, new_acceptance_rate, step + 1
+        )
 
-    # do this 10-100 time
-    gradient = target_acceptance_rate - acceptance_rate
-    sigma, adam_state = adam_update(adam_state, sigma, gradient)
+    def final(warmup_state: RWMHWarmupState):
+        return warmup_state.parameter
