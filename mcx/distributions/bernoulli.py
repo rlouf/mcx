9253473d986aefbf5bbb7ccb08c8a6af077082a8
Rémi Louf <remilouf@gmail.com>
2021-02-19 22:08:16 +0100

    simplify bernoulli distirbution

--- mcx/distributions/bernoulli.py
+++ mcx/distributions/bernoulli.py
@@ -3,7 +3,6 @@ from jax import random
 from jax.scipy.special import xlog1py, xlogy
 from mcx.distributions import constraints
 from mcx.distributions.distribution import Distribution
-from mcx.distributions.shapes import promote_shapes
 
 
 class Bernoulli(Distribution):
@@ -12,9 +11,8 @@ class Bernoulli(Distribution):
 
     def __init__(self, p):
         self.event_shape = ()
-        batch_shape, (p,) = promote_shapes(p)
-        self.batch_shape = batch_shape
-        self.p = jnp.broadcast_to(p * 1.0, batch_shape)  # will fail if p is int
+        self.batch_shape = jnp.shape(p)
+        self.p = p * 1.0  # will fail if p is int
 
     def sample(self, rng_key, sample_shape=()):
         shape = sample_shape + self.batch_shape + self.event_shape
