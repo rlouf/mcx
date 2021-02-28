b84bc87d8471b8819f43d803cc15dc06db7ced4a
Tim Blazina <tim.blazina@gmail.com>
2021-02-28 09:53:24 +0100

    Implement suggested changes from code review
    1) Use `jnp` instead of `np` for jax numpy
    2) Use promote shape function on class parameters
    3) Expliticly broadcast batch shape and parameter shapes

--- mcx/distributions/betabinomial.py
+++ mcx/distributions/betabinomial.py
@@ -1,11 +1,12 @@
-from jax import numpy as np
+from jax import lax
+from jax import numpy as jnp
 from jax import random
 from jax.scipy import stats
 
 from mcx.distributions import constraints
 from mcx.distributions.binomial import _random_binomial
 from mcx.distributions.distribution import Distribution
-from mcx.distributions.shapes import broadcast_batch_shape
+from mcx.distributions.shapes import promote_shapes
 
 
 class BetaBinomial(Distribution):
@@ -18,15 +19,17 @@ class BetaBinomial(Distribution):
     def __init__(self, n, a, b):
         self.support = constraints.integer_interval(0, n)
         self.event_shape = ()
-        self.batch_shape = broadcast_batch_shape(np.shape(n), np.shape(a), np.shape(b))
-        self.a = a
-        self.b = b
-        self.n = n
+        n, a, b = promote_shapes(n, a, b)
+        batch_shape = lax.broadcast_shapes(jnp.shape(n), jnp.shape(a), jnp.shape(b))
+        self.batch_shape = batch_shape
+        self.a = jnp.broadcast_shape(a, batch_shape)
+        self.b = jnp.broadcast_shape(b, batch_shape)
+        self.n = jnp.broadcast_shape(n, batch_shape)
 
     def sample(self, rng_key, sample_shape=()):
         shape = sample_shape + self.batch_shape + self.event_shape
         p = random.beta(rng_key, self.a, self.b, shape=shape)
-        n_max = np.max(self.n).item()
+        n_max = jnp.max(self.n).item()
         return _random_binomial(rng_key, p, self.n, n_max, shape)
 
     @constraints.limit_to_support
