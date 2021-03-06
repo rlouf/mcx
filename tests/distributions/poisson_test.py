6baf8c6a9bf2a6d05e20077d18ce1a50ea90cb78
mattKretschmer <matthew.kretschmer@gmail.com>
2021-03-05 22:18:55 -0500

    black of poisson_test

--- tests/distributions/poisson_test.py
+++ tests/distributions/poisson_test.py
@@ -35,24 +35,41 @@ def test_logpdf_out_of_support(case):
 # SAMPLING SHAPE
 #
 
-@pytest.mark.parametrize(["lmbda", "sample_shape", "expected_shape"], [
-    [jnp.array(1), (5,), (5,)], # 5 1d samples
-    [jnp.array([1,2]), (5,), (5,2)], # 5 samples from 2 poisson distributions
-    [jnp.array([1,2]), (5,2), (5,2,2)],
-    [jnp.array([1,2,0,0]), (10,), (10, 4)], # 10 samples from 4 poisson distributions
-    [jnp.array([[1,2], [5,10]]), (5,2), (5,2,2,2)], # 10 samples from a 2x2 batch of Poissons.
-])
+
+@pytest.mark.parametrize(
+    ["lmbda", "sample_shape", "expected_shape"],
+    [
+        [jnp.array(1), (5,), (5,)],  # 5 1d samples
+        [jnp.array([1, 2]), (5,), (5, 2)],  # 5 samples from 2 poisson distributions
+        [jnp.array([1, 2]), (5, 2), (5, 2, 2)],
+        [
+            jnp.array([1, 2, 0, 0]),
+            (10,),
+            (10, 4),
+        ],  # 10 samples from 4 poisson distributions
+        [
+            jnp.array([[1, 2], [5, 10]]),
+            (5, 2),
+            (5, 2, 2, 2),
+        ],  # 10 samples from a 2x2 batch of Poissons.
+    ],
+)
 def test_sampling_shape(lmbda, expected_shape, sample_shape, rng_key):
     assert Poisson(lmbda=lmbda).sample(rng_key, sample_shape).shape == expected_shape
 
 
-@pytest.mark.parametrize(["lmbda", "expected_shape"], [
-    [jnp.array(1), ()], # 1 sample from 1d poisson
-    [jnp.array([1, 2]), (2,)], # 1 sample from 2 independent poissons
-    [jnp.array([[1, 2], [5,10]]), (2,2)], # 2 samples from 2 poissons
-])
-def test_sampling_noshape(lmbda, expected_shape,rng_key):
+@pytest.mark.parametrize(
+    ["lmbda", "expected_shape"],
+    [
+        [jnp.array(1), ()],  # 1 sample from 1d poisson
+        [jnp.array([1, 2]), (2,)],  # 1 sample from 2 independent poissons
+        [jnp.array([[1, 2], [5, 10]]), (2, 2)],  # 2 samples from 2 poissons
+    ],
+)
+def test_sampling_noshape(lmbda, expected_shape, rng_key):
     assert Poisson(lmbda=lmbda).sample(rng_key).shape == expected_shape
+
+
 #
 # LOGPDF SHAPES
 #
